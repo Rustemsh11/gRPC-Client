3fa4ec18bd446ad76e8b70bb7678c391da222719
Rustem Shaiheraziev <mr.shaykheraziev@mail.ru>
2023-10-05 08:24:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Concurrent;
+using System;
+using Grpc.Net.Client;
+using ConsoleApp1;
+using Stream;
+using Grpc.Core;
+
+
+//Console.WriteLine("Press any key to start processing");
+//Console.ReadKey();
+//Console.WriteLine("Processing");
+//Task.Factory.StartNew(ProcessIntData);
+
+//var chanel = GrpcChannel.ForAddress("https://localhost:7257");
+//var client = new Inviter.InviterClient(chanel);
+
+//var response = await client.InviteAsync(new Request { Name = "Rustem" });
+
+//var eventInvokation = response.Invitation;
+//var eventDateTime = response.Start.ToDateTime();
+//var eventDuration = response.Duration.ToTimeSpan();
+
+//Console.WriteLine(eventInvokation);
+//Console.WriteLine($"Начало: {eventDateTime.ToString("dd:MM:HH:mm")}  Длительность:{eventDuration.TotalHours} часа");
+
+string[] messages = { "Привет", "Как дела?", "Че молчишь?", "Ты че, спишь", "Ну пока" };
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7257");
+var client = new Messenger.MessengerClient(channel);
+
+var call = client.DataStream();
+
+var readTask = Task.Run(async () =>
+{
+    await foreach (var response in call.ResponseStream.ReadAllAsync())
+    {
+        Console.WriteLine($"Server: {response.Content}");
+    }
+});
+
+foreach (var message in messages)
+{
+    await call.RequestStream.WriteAsync(new Stream.Request { Content = message });
+    Console.WriteLine(message);
+    await Task.Delay(2000);
+
+}
+await call.RequestStream.CompleteAsync();
+await readTask;
+
+
+Console.ReadLine();
+
+
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.CompilerServices;
+
+var teachers = new List<Teacher>(); // Заполнить из файла Учетиля.txt
+var students = new List<Student>(); // Заполнить из файла Ученики.txt
+var exams = new List<Exams>();
+
+teachers = GetTeacherFromFile(@"C:\Users\IT\source\repos\ConsoleApp1\ConsoleApp2\Teacher.txt");
+
+//1. Найти учителя у которого в классе меньше всего учеников 
+//2. Найти средний бал экзамена по Физики за 2023 год.		
+//3. Получить количество учиников которые по экзамену Математики получили больше 90 баллов, где учитель AleList
+List<Teacher> GetTeacherFromFile(string path)
+{
+    var teachers = new List<Teacher>();
+    string[] lines = File.ReadAllLines(path);
+    for (int i = 1; i < lines.Length; i++)
+    {
+        if (!string.IsNullOrEmpty(lines[i].Split("\t")[0]) || !string.IsNullOrEmpty(lines[i].Split("\t")[1]) || !string.IsNullOrEmpty(lines[i].Split("\t")[2]))
+        {
+                teachers.Add(new Teacher
+                {
+                    Name = lines[i].Split("\t")[0],
+                    LastName = lines[i].Split("\t")[1],
+                    Age = Convert.ToInt16(lines[i].Split("\t")[2])
+                });
+        }
+
+    } 
+    return teachers;
+}
+public class Person
+{
+    public long ID { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
+
+public class Teacher : Person
+{
+    public LessonType Lesson { get; set; }
+}
+
+public class Student : Person
+{
+
+}
+
+public class Exams
+{
+    public LessonType Lesson { get; set; }
+
+    public long StudentId { get; set; }
+    public long TeacherId { get; set; }
+
+    public decimal Score { get; set; }
+    public DateTime ExamDate { get; set; }
+
+    public Student Student { get; set; }
+    public Teacher Teacher { get; set; }
+}
+
+public enum LessonType
+{
+    Mathematics = 1,
+    Physics = 2
+}
